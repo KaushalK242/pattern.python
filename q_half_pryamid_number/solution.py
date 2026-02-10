a9535dc80bc349f0d05a703ff5bd38f25e8c6682
Kaushal Kumar <kaushalroy792@gmail.com>
2026-02-10 23:21:03 +0530

    Added star pattern

---
+++ q_half_pryamid_number/solution.py
@@ -0,0 +1,13 @@
+#print pattern
+
+#input of row and column
+
+m = int(input("Enter the number of rows: "))
+
+
+for i in range (1,m+1):
+    for j in range(i):
+        print(i , end="")
+    print()
+
+       
